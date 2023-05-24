e2fa579043aab328fb91ef817415fde2d9c40b59
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-25 03:38:25 +0530

    Nav Bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             <EmailOutlinedIcon/>
             <NotificationsNoneOutlinedIcon/>
             <div className="user">
-                {/* <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /> */}
+                <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
                 <span>Vinal Munasinghe</span>
 
 

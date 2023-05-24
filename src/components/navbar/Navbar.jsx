ae785c1be39dc6457c42ae5cb934e161543e9206
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-25 02:48:46 +0530

    UI Insertion

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,8 @@
 import "./Navbar.scss"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 
 const Navbar = () => {
     return (

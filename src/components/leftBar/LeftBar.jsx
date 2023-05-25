243fbe275294637f33a4bf893bdc707f948504c9
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-26 02:18:08 +0530

    Update LeftBar.jsx

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,8 +1,24 @@
-import "./LeftBar.scss"
+import "./leftBar.scss"
 
 const LeftBar = () => {
     return (
-        <div className="leftBar"> LeftBar </div>    
+        <div className="leftBar"> 
+             <div className="container"> 
+                     <div className="menu">
+                          <div className="user">
+                          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                          <span>Vinal Munasinghe</span>
+                            
+                            
+                             </div>
+                             <div className="item">
+                                <img src="" alt="" />
+                                <span>Friends</span>
+                             </div>
+                         </div>
+                 </div>
+           
+            </div>    
     )
 }
 

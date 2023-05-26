dd9336a0dd4945631ba9d52018530711884a8136
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-26 19:31:52 +0530

    RightBar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -13,12 +13,70 @@ const RightBar = () => {
                             <span>Vinal Munasinghe</span>
                         </div>
                         <div className="buttons">
-                            <button>follow</button>
-                            <button>reject</button>
+                            <button>Follow</button>
+                            <button>Reject</button>
+
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <span>Vinal Munasinghe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Follow</button>
+                            <button>Reject</button>
 
                         </div>
                     </div>
 
+                </div>
+                <div className="item">
+                    <span>Latest Activities</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <p>
+                                <span>Vinal   </span>
+                                  changed their cover photo
+                            </p>
+                        </div>
+                        <span>1 minute ago</span>
+                    </div>
+
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <p>
+                                <span>Vinal   </span>
+                                  Liked a Post
+                            </p>
+                        </div>
+                        <span>1 minute ago</span>
+                    </div>
+
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <p>
+                                <span>Vinal   </span>
+                                  Commented on a Post
+                            </p>
+                        </div>
+                        <span>1 minute ago</span>
+                    </div>
+
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <p>
+                                <span>Vinal   </span>
+                                  Posted a Picture
+                            </p>
+                        </div>
+                        <span>1 minute ago</span>
+                    </div>
+                    
                 </div>
                 </div>  
         </div>

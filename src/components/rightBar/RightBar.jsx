99d9c4133abf0681afd59bfafda2ce4fbf632c22
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-26 18:48:02 +0530

    RightBar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,8 +1,27 @@
+import { ImageAspectRatioSharp } from "@mui/icons-material"
 import "./rightBar.scss"
 
 const RightBar = () => {
     return (
-        <div className="rightBar">RightBar</div>
+        <div className="rightBar">
+            <div className="container">
+                <div className="item">
+                    <span className="title">Suggestions for you</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <span>Vinal Munasinghe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>reject</button>
+
+                        </div>
+                    </div>
+
+                </div>
+                </div>  
+        </div>
     )
 }
 

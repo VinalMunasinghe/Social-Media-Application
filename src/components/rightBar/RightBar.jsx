7018973e0c7c2c1b22cfcc99acb8ffa43f37410f
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-26 19:59:03 +0530

    Rightbar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -78,6 +78,69 @@ const RightBar = () => {
                     </div>
                     
                 </div>
+                <div className="item">
+                    <span>Online Friends</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+
+                            <div className="online"  />
+
+
+                                <span>Vinal   </span>   
+                            
+                        </div>
+                    </div>
+
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+
+                            <div className="online"  />
+
+
+                                <span>Vinal   </span>   
+                            
+                        </div>
+                    </div>
+
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+
+                            <div className="online"  />
+
+
+                                <span>Vinal   </span>   
+                            
+                        </div>
+                    </div>
+
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+
+                            <div className="online"  />
+
+
+                                <span>Vinal   </span>   
+                            
+                        </div>
+                    </div>
+
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+
+                            <div className="online"  />
+
+
+                                <span>Vinal   </span>   
+                            
+                        </div>
+                    </div>
+
+                </div>
                 </div>  
         </div>
     )
